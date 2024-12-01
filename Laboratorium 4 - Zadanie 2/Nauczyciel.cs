c343a1879d03bdbaa9a1274874d9a7e645676726
Łukasz Filipek <lukasz3dx@gmail.com>
2024-12-01 22:00:15 +0100

    Laboratorium 4

---
+++ Laboratorium 4 - Zadanie 2/Nauczyciel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorium_4___Zadanie_2
+{
+    public class Nauczyciel : Osoba
+    {
+        private string tytulNaukowy;
+        private List<Uczen> uczniowieKlasy;
+
+        public Nauczyciel(string imie, string nazwisko, string pesel, string tytulNaukowy) : base(imie, nazwisko, pesel)
+        {
+            this.tytulNaukowy = tytulNaukowy;
+            uczniowieKlasy = new List<Uczen>();
+        }
+
+        public void DodajUcznia(Uczen uczen)
+        {
+            uczniowieKlasy.Add(uczen);
+        }
+
+        public List<Uczen> KtorzyUcziowieMogaWrocicSamodzielnie()
+        {
+            var uczniowieSamodzielni = new List<Uczen>();
+            foreach (var uczen in uczniowieKlasy)
+            {
+                if (uczen.CzyMozeSamWrocicDoDomu())
+                {
+                    uczniowieSamodzielni.Add(uczen);
+                }
+            }
+            return uczniowieSamodzielni;
+        }
+
+        public void PodsumowanieKlasy(DateTime data)
+        {
+            Console.WriteLine($"Dnia: {data.ToShortDateString()}");
+            Console.WriteLine($"Nauczyciel: {tytulNaukowy} {PobierzPelneImieNazwisko()}");
+            Console.WriteLine("Lista studentów:");
+
+            int lp = 1;
+            foreach (var uczen in uczniowieKlasy)
+            {
+                string pelneImieNazwisko = uczen.PobierzPelneImieNazwisko();
+                string plec = uczen.PobierzPlec();
+                string mozeSam = uczen.CzyMozeSamWrocicDoDomu() ? "Może wrócić sam" : "Nie może wrócić sam";
+                Console.WriteLine($"{lp}. {pelneImieNazwisko} - {plec} - {mozeSam}");
+                lp++;
+            }
+        }
+    }
+}
